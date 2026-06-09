cce07b650d34752aafc7a1986044b3e49d0c7982
heziiop <q_m_p@qq.com>
2026-06-10 01:30:44 +0800

    Update run.sh

--- 8卡混部-128k1k-0.9prefixcache-低试延/run.sh
+++ 8卡混部-128k1k-0.9prefixcache-低试延/run.sh
@@ -9,62 +9,57 @@ unset HTTPS_PROXY
 unset HTTP_PROXY
 unset ASCEND_LAUNCH_BLOCKING
 
+export PYTHONPATH=/home/xxx/code/sglang/python:$PYTHONPATH
+
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 source /usr/local/Ascend/nnal/atb/set_env.sh
-# 内存碎片
 export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
 export STREAMS_PER_DEVICE=32
-# 网卡
 export HCCL_SOCKET_IFNAME=lo
 export GLOO_SOCKET_IFNAME=lo
 
-#export HCCL_OP_EXPANSION_MODE=AIV
 export TASK_QUEUE_ENABLE=1
 
 export ASCEND_USE_FIA=1
-export HCCL_BUFFSIZE=1600
 export SGLANG_SET_CPU_AFFINITY=1
 export SGLANG_ENABLE_SPEC_V2=1
 export SGLANG_ENABLE_OVERLAP_PLAN_STREAM=1
-export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=640
-export DEEPEP_NORMAL_LONG_SEQ_ROUND=64
-export DEEPEP_NORMAL_LONG_SEQ_PER_ROUND_TOKENS=2048
-export DEEPEP_NORMAL_COMBINE_ENABLE_LONG_SEQ=1
 export SGLANG_NPU_FUSED_MOE_MODE=2
-export SGLANG_NPU_DEEPEP_USE_FUSED_MOE_DECODE=1
-export SGLANG_NPU_FUSEEP_DECODE_ONLY=1
+export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=160000
+
+export DEEP_NORMAL_MODE_USE_INT8_QUANT=1
 
 
-export ENABLE_PROFILING=0
-export PROFILING_BS=28
-export PROFILING_STAGE="decode"
-export PROFILING_step=10
+export HCCL_BUFFSIZE=1024
 
 
-export PYTHONPATH=/home/weights/MiniMax-M2.5-eagel-model-0318-upload:$PYTHONPATH
+MODEL_PATH=/home/weights/MiniMax-M2.5-w8a8-QuaRot
+EAGLE_MODEL_PATH=/home/weights/MiniMax-M2.5-eagel-model-0318
+export PYTHONPATH=${EAGLE_MODEL_PATH}:$PYTHONPATH
 export SGLANG_EXTERNAL_MODEL_PACKAGE=custom_eagle3
 
-sglang serve \
-   --model-path /home/weights/MiniMax-M2.5-w8a8-QuaRot \
+python -m sglang.launch_server \
+   --model-path $MODEL_PATH \
    --host 127.0.0.1 \
-   --port 32000 \
+   --port 6677 \
    --tp-size 16 \
    --dp-size 2 \
    --enable-dp-attention \
-   --prefill-delayer-max-delay-passes 100 \
-   --enable-prefill-delayer \
    --mem-fraction-static 0.65 \
-   --max-running-requests 8 \
-   --chunked-prefill-size -1 --max-prefill-token 130000 \
-   --cuda-graph-bs 1 2 4 \
+   --max-running-requests 4 \
+   --reasoning-parser minimax-append-think \
+   --tool-call-parser minimax-m2 \
+   --enable-prefill-delayer \
+   --prefill-max-requests 4 \
+   --chunked-prefill-size 160000 \
+   --max-prefill-token 80000 \
+   --cuda-graph-bs 2 4 6 8 \
    --moe-a2a-backend ascend_fuseep --deepep-mode auto --quantization modelslim \
    --speculative-algorithm EAGLE3 \
-   --speculative-draft-model-path /home/weights/MiniMax-M2.5-eagel-model-0318-upload \
+   --speculative-draft-model-path $EAGLE_MODEL_PATH \
    --speculative-num-steps 3 \
    --speculative-eagle-topk 1 \
    --speculative-num-draft-tokens 4 \
    --speculative-draft-model-quantization unquant \
-   --dtype bfloat16 \
-   --trust-remote-code \
-   --tokenizer-worker-num 8 \
-
+   --tokenizer-worker-num 4 \
+   --dtype bfloat16
